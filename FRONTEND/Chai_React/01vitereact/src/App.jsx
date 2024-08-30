bc6440cb98add9f8b018d9661a00ec5ded9a9d88
Taniy0819 <taniya00019@gmail.com>
2024-08-30 20:23:32 +0530

    Chai Aur React

---
+++ FRONTEND/Chai_React/01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Chai from './Chai';
+
+function App() {
+
+  return (
+    <>
+    <h1>Chai aur react | Taniya</h1>
+    <Chai/>
+    </>
+  );
+}
+
+export default App;
